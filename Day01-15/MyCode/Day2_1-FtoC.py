4d5ee470aa38bdc24fad511982cff315a72ce3f7
JiepengLab <1978264748@qq.com>
2023-10-12 00:21:25 +0800

    Day 2

---
+++ Day01-15/MyCode/Day2_1-FtoC.py
@@ -0,0 +1,3 @@
+F = float(input("F="))
+C = (F - 32) / 1.8
+print("%.1f F = %.1f C" % (F,C))
---
+++ Day01-15/MyCode/Day2_2-RtoC&S.py
@@ -0,0 +1,4 @@
+import math
+
+R = float(input("R = "))
+print("C = %.1f, S = %.1lf"%(2*math.pi*R,math.pi*R*R))
